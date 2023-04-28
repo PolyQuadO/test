54145de2d323489afc5f3290212dfbde95b29b39
taeiljung <brilliant_427@naver.com>
2023-04-28 11:27:10 +0900

    가져옴 및 코드정리

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:table_calendar/table_calendar.dart'; // TABLECALENDAR 추가
-// import 'package:firebase_database/firebase_database.dart';
 import 'firebase_options.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'package:test0415/LoginForm/Login.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//
-//   runApp(AuthAndDatabaseExample());
-// }
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   // 날짜와 시간의 형식을 초기화
   await initializeDateFormatting();
-
   // Firebase 초기화
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
